d3c50f84f4509d3da9a96f424c21df2d1fbe17fb
Whee99 <41610471+Whee99@users.noreply.github.com>
2022-10-02 22:42:00 +0700

    Initial commit

---
+++ LinkedList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] num = { 1, 2, 3, 4, 5 };
+        LinkedList<int> list = new LinkedList<int>(num);
+
+        LinkedListNode<int> currentNode = list.First;
+
+        foreach (int i in list)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine("");
+
+        list.AddFirst(0);
+        list.AddLast(6);
+
+        foreach (int i in list)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine("");
+
+        while (currentNode != null)
+        {
+            if(currentNode.Value == 4) { 
+                list.AddAfter(currentNode, 9);
+                break;
+            }
+            currentNode = currentNode.Next;
+        }
+
+        foreach (int i in list)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine("");
+
+        while (currentNode != null)
+        {
+            if(currentNode.Value == 5)
+            {
+                LinkedListNode<int> temp = currentNode;
+                currentNode = currentNode.Next;
+                list.Remove(temp);
+            }
+            else
+            {
+                currentNode = currentNode.Next;
+            }
+        }
+
+        foreach (int i in list)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
