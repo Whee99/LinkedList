1908519a4ccaafd2cd5b59e1ef68d4a143fe6c76
Whee99 <41610471+Whee99@users.noreply.github.com>
2022-10-04 20:57:01 +0700

    New problem
    New nodes aren't linked???

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -2,28 +2,57 @@
 using System.Collections.Generic;
 
 public class Program
-{
+{ // start of class Program
     public static void Main(string[] args)
-    {
+    { // start of Main
         int[] num = { 1, 2, 3, 4, 5 };
         LinkedList<int> list = new LinkedList<int>(num);
 
         LinkedListNode<int> currentNode = list.First;
 
-        foreach (int i in list)
+        void print_list(LinkedList<int> list)
         {
-            Console.Write($"{i} ");
+            foreach (int i in list)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("");
         }
-        Console.WriteLine("");
+
+        void current_node_traits()
+        { // start of current_node_traits
+            if (currentNode == null)
+            {
+                Console.WriteLine("Node is null");
+            }
+            else
+            {
+                Console.WriteLine($"Current Node: {currentNode.Value}");
+
+                if (currentNode.Previous == null) {
+                    Console.WriteLine("This node is the head");
+                }
+                else {
+                    Console.WriteLine($"Previous Node: {currentNode.Previous.Value}");
+                }
+
+                if(currentNode.Next == null) {
+                    Console.WriteLine("This node is the tail");
+                }
+                else {
+                    Console.WriteLine($"Next Node: {currentNode.Next.Value}");
+                }
+            }
+        } // end of current_node_traits
+
+        print_list(list);
 
         list.AddFirst(0);
         list.AddLast(6);
 
-        foreach (int i in list)
-        {
-            Console.Write($"{i} ");
-        }
-        Console.WriteLine("");
+        print_list(list);
+
+        current_node_traits();
 
         while (currentNode != null)
         {
@@ -34,11 +63,8 @@ public class Program
             currentNode = currentNode.Next;
         }
 
-        foreach (int i in list)
-        {
-            Console.Write($"{i} ");
-        }
-        Console.WriteLine("");
+        print_list(list);
+        Console.WriteLine($"Current Node: {currentNode.Value}");
 
         while (currentNode != null)
         {
@@ -54,12 +80,10 @@ public class Program
             }
         }
 
-        foreach (int i in list)
-        {
-            Console.Write($"{i} ");
-        }
-        Console.WriteLine("");
+        print_list(list);
+
+        current_node_traits();
 
         Console.WriteLine($"List's length: {list.Count}");
-    }
-}
+    } // end of Main
+} // end of class Program
